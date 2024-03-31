48a426b0cd7944c844e7fb4cbe481f3db57c4bb3
Devyn Freet <devyn@devynfreet.com>
2024-03-31 12:59:47 -0400

    added linear path generation

--- main.lua
+++ main.lua
@@ -2,9 +2,21 @@ local border = 50
 local squareSize = 10
 local gap = 1
 local rows, columns = 50,50
+local pathType = {LINEAR=0, EFFICIENT=1, RANDOM=2}
 
 local tiles
 local enter, exit
+
+local function generatePath(type)
+    if type == pathType.LINEAR then     -- Assumes enter and exit are along upper and lower walls respectively
+        for y=enter.y+1,exit.y-1 do
+            tiles[y][enter.x] = 4
+        end
+        for x=enter.x,exit.x do
+            tiles[exit.y-1][x] = 4
+        end
+    end
+end
 
 function love.load()
     love.window.setMode(border *2 + (squareSize + gap) * rows, border * 2 + (squareSize + gap) * columns)
@@ -33,6 +45,7 @@ function love.load()
     
     tiles[enter.y][enter.x] = 2
     tiles[exit.y][exit.x] = 3
+    generatePath(pathType.LINEAR)
 end
 
 function love.draw()
@@ -44,6 +57,8 @@ function love.draw()
                 love.graphics.setColor(1,0,0)
             elseif w == 3 then
                 love.graphics.setColor(0,1,0)
+            elseif w == 4 then
+                love.graphics.setColor(0,0,1)
             end
             if w ~= 0 then
                 love.graphics.rectangle("fill", j * (squareSize + gap) + border, i * (squareSize + gap) + border, squareSize, squareSize)
